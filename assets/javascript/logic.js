c7e0735e2fac867bddfdd6bcf75e0fbbdea3527c
Jonathan King <imjonathanking@gmail.com>
2019-04-13 11:34:36 -0400

    table working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,6 +3,7 @@ console.log("working");
 //Dependencies
 // =============================================================
 var express = require("express");
+var path = require("path");
 
 //Array of objects for reservations
 var reservations = require("./reservations.js");
@@ -12,6 +13,7 @@ var reservations = require("./reservations.js");
 var app = express();
 var PORT = 3000;
 
+
 // Sets up the Express app to handle data parsing
 // =============================================================
 app.use(express.urlencoded({ extended: true }));
@@ -20,6 +22,14 @@ app.use(express.json());
 //Returning JSON data for all reservations
 app.get("/api/reservations", function(req, res) {
     return res.json(reservations);
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../index.html"));
+});
+
+app.get("/view", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../view.html"));
 });
 
 
